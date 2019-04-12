43afef06a337d187b5705eab0ded83733887674b
imabuza <iitmabuza@gmail.com>
2019-04-12 15:28:51 +0200

    added box office

--- rotten_scrape.py
+++ rotten_scrape.py
@@ -58,6 +58,51 @@ df.to_csv("moviesOpeningThisWeek.csv")
 
 print("wrote to CSV")
 
+print(percentage_list,title_list)
+
+
+#get box office rating
+
+boxOffice = div_listings[1]
+
+left_col = boxOffice.table.findAll("td", {"class": "left_col"})
+middle_col = boxOffice.table.findAll("td", {"class": "middle_col"})
+right_col = boxOffice.table.findAll("td", {"class": "right_col"})
+
+percentage_list = []
+title_list = []
+gross_list = []
+
+for items in left_col:
+    try:
+        Percentage = items.findAll("span",{"class":"tMeterScore"})
+        moviePercentage = Percentage[0].text
+        moviePercentage = moviePercentage.replace("%","")
+        percentage_list.append(int(moviePercentage))
+
+    except:
+        percentage_list.append(0)
+
+for items in middle_col:
+    title = items.a.text
+    title_list.append(title)
+
+for items in right_col:
+    gross = items.a.text
+    gross = gross.replace("M","")
+    gross = gross.replace("$","")
+    gross_list.append(gross)
+
+data = {"Movies": title_list, "Percentage": percentage_list, "Gross": gross_list}
+
+df = pd.DataFrame.from_dict(data)
+
+df.to_csv("boxoffice.csv")
+
+print("wrote to box Office CSV")
+
+print(percentage_list,title_list, gross_list)
+
 
 
 
